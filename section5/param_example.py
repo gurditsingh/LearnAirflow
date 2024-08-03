67942d3e1afe7639bc6485697e1c721941df8415
gurditsingh <gurdit.singh.041@gmail.com>
2024-08-03 23:59:37 +0530

    rename

---
+++ section5/param_example.py
@@ -0,0 +1,52 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+
+def print_params(**kwargs):
+    params = kwargs.get('params', {})
+    print(f"Param1: {params.get('param1', 'default_value')}")
+    print(f"Param2: {params.get('param2', 0)}")
+    print(f"Param3: {params.get('param3', [])}")
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+}
+
+dag = DAG(
+    'dag_with_params',
+    default_args=default_args,
+    description='A DAG with params for tasks',
+    schedule_interval=None,
+    params={
+        'param1': 'dag_value1',   # DAG level
+        'param2': 42,
+        'param3': [1, 2, 3]
+    }
+)
+
+print_params_task = PythonOperator(
+    task_id='print_params_task',
+    python_callable=print_params,
+    provide_context=True,
+    params={
+        'param1': 'task_value1',  # Task level
+        'param2': 99
+    },
+    dag=dag
+)
+
+print_params_bash_task = BashOperator(
+    task_id='print_params_bash_task',
+    bash_command="""echo "Param1: {{ params.param1 }}"
+                    echo "Param2: {{ params.param2 }}"
+                    echo "Param3: {{ params.param3 }}" """,
+    params={
+        'param1': 'bash_value1'   # Task level
+    },
+    dag=dag
+)
+
+print_params_task
+print_params_bash_task
